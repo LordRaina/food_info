579c6e43334099011a19f94c0e0896a5034d8baa
Jacky Trinh <trinhjacky@yahoo.fr>
2023-01-08 23:08:12 +0100

    Remove potential e2e testing because no time to continue

--- app/src/food-facts/food-facts.controller.ts
+++ app/src/food-facts/food-facts.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
-import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { FoodFactsService } from './food-facts.service';
 
 
